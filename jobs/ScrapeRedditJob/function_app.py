b4028a6f6c64c449019cdd93a538d643390ffaee
steven20210 <stevenli7210@gmail.com>
2025-09-08 10:35:53 -0400

    added trigger tes

--- jobs/ScrapeRedditJob/function_app.py
+++ jobs/ScrapeRedditJob/function_app.py
@@ -9,7 +9,6 @@ app = func.FunctionApp()
 @app.timer_trigger(schedule="0 */1 * * * *", arg_name="myTimer", run_on_startup=True,
               use_monitor=False) 
 def ScrapeRedditJob(myTimer: func.TimerRequest) -> None:
-    
     if myTimer.past_due:
         logging.info('The timer is past due!')
     logging.info("Starting Reddit scraping job")
