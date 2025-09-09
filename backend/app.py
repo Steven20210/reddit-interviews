3323e8de8bf61729ae534757a2a9c336f57556d9
steven20210 <stevenli7210@gmail.com>
2025-09-08 23:11:41 -0400

    retrigger deploy

--- backend/app.py
+++ backend/app.py
@@ -120,5 +120,5 @@ def search(request: Request, search_request: SearchRequest, token: str = Depends
     }
     
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))  # fallback to 8000 locally
+    port = int(os.environ.get("PORT", 8080))  # fallback to 8000 locally
     uvicorn.run(app, host="0.0.0.0", port=port)
